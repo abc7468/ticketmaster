b0af72edc4637d79195efe5752077267eba6cb05
SeongHyun <abc7468@naver.com>
2022-06-06 21:23:26 +0900

    feat : dblayer

--- src/lib/persistence/dblayer/dblayer.go
+++ src/lib/persistence/dblayer/dblayer.go
@@ -1,1 +1,22 @@
 package dblayer
+
+import (
+	"ticketmaster/src/lib/persistence"
+	"ticketmaster/src/lib/persistence/mongolayer"
+)
+
+type DBTYPE string
+
+const (
+	MONGODB  DBTYPE = "mongodb"
+	DYNAMODB DBTYPE = "dynamodb"
+)
+
+func NewPersistenceLayer(options DBTYPE, connection string) (persistence.DatabaseHandler, error) {
+
+	switch options {
+	case MONGODB:
+		return mongolayer.NewMongoDBLayer(connection)
+	}
+	return nil, nil
+}
